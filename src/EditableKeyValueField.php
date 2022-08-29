3dc1f35439231db0bc6434be1a9c1c218c75bbf1
Will Rossiter <will@fullscreen.io>
2022-08-29 14:03:26 +1200

    fix: wrong logic

--- src/EditableKeyValueField.php
+++ src/EditableKeyValueField.php
@@ -6,7 +6,7 @@ use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\TextareaField;
 use SilverStripe\UserForms\Model\EditableFormField;
 
-if (class_exists(EditableFormField::class)) {
+if (!class_exists(EditableFormField::class)) {
     return;
 }
 
